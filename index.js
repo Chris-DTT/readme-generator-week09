cae3aa1a285f85043d957cf9f36732e4d56505cf
Christian Romero <christianromero.work@gmail.com>
2022-08-06 12:41:31 -0500

    Fix license links to now properly link; add markdown generate template

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -76,69 +76,69 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   switch (license) {
     case "Apache 2.0 License":
-      return "https://opensource.org/licenses/Apache-2.0";
+      return "[https://opensource.org/licenses/Apache-2.0](https://opensource.org/licenses/Apache-2.0)";
     case "Boost Software License 1.0":
-      return "https://www.boost.org/LICENSE_1_0.txt";
+      return "[https://www.boost.org/LICENSE_1_0.txt](https://www.boost.org/LICENSE_1_0.txt)";
     case "BSD 3-Clause License":
-      return "https://opensource.org/licenses/BSD-3-Clause";
+      return "[https://opensource.org/licenses/BSD-3-Clause](https://opensource.org/licenses/BSD-3-Clause)";
     case "BSD 2-Clause License":
-      return "https://opensource.org/licenses/BSD-2-Clause";
+      return "[https://opensource.org/licenses/BSD-2-Clause](https://opensource.org/licenses/BSD-2-Clause)";
     case "CC0":
-      return "http://creativecommons.org/publicdomain/zero/1.0/";
+      return "[http://creativecommons.org/publicdomain/zero/1.0/](http://creativecommons.org/publicdomain/zero/1.0/)";
     case "Attribution 4.0 International":
-      return "https://creativecommons.org/licenses/by/4.0/";
+      return "[https://creativecommons.org/licenses/by/4.0/](https://creativecommons.org/licenses/by/4.0/)";
     case "Attribution-ShareAlike 4.0 International":
-      return "https://creativecommons.org/licenses/by-sa/4.0/";
+      return "[https://creativecommons.org/licenses/by-sa/4.0/](https://creativecommons.org/licenses/by-sa/4.0/)";
     case "Attribution-NonCommercial 4.0 International":
-      return "https://creativecommons.org/licenses/by-nc/4.0/";
+      return "[https://creativecommons.org/licenses/by-nc/4.0/](https://creativecommons.org/licenses/by-nc/4.0/)";
     case "Attribution-NoDerivates 4.0 International":
-      return "https://creativecommons.org/licenses/by-nd/4.0/";
+      return "[https://creativecommons.org/licenses/by-nd/4.0/](https://creativecommons.org/licenses/by-nd/4.0/)";
     case "Attribution-NonCommmercial-ShareAlike 4.0 International":
-      return "https://creativecommons.org/licenses/by-nc-sa/4.0/";
+      return "[https://creativecommons.org/licenses/by-nc-sa/4.0/](https://creativecommons.org/licenses/by-nc-sa/4.0/)";
     case "Attribution-NonCommercial-NoDerivatives 4.0 International":
-      return "https://creativecommons.org/licenses/by-nc-nd/4.0/";
+      return "[https://creativecommons.org/licenses/by-nc-nd/4.0/](https://creativecommons.org/licenses/by-nc-nd/4.0/)";
     case "Eclipse Public License 1.0":
-      return "https://opensource.org/licenses/EPL-1.0";
+      return "[https://opensource.org/licenses/EPL-1.0](https://opensource.org/licenses/EPL-1.0)";
     case "GNU GPL v3":
-      return "https://www.gnu.org/licenses/gpl-3.0";
+      return "[https://www.gnu.org/licenses/gpl-3.0](https://www.gnu.org/licenses/gpl-3.0)";
     case "GNU GPL v2":
-      return "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+      return "[https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
     case "GNU AGPL v3":
-      return "https://www.gnu.org/licenses/agpl-3.0";
+      return "[https://www.gnu.org/licenses/agpl-3.0](https://www.gnu.org/licenses/agpl-3.0)";
     case "GNU LGPL v3":
-      return "https://www.gnu.org/licenses/lgpl-3.0";
+      return "[https://www.gnu.org/licenses/lgpl-3.0](https://www.gnu.org/licenses/lgpl-3.0)";
     case "GNU FDL v1.3":
-      return "https://www.gnu.org/licenses/fdl-1.3";
+      return "[https://www.gnu.org/licenses/fdl-1.3](https://www.gnu.org/licenses/fdl-1.3)";
     case "The Hippocratic License 2.1":
-      return "https://firstdonoharm.dev";
+      return "[https://firstdonoharm.dev](https://firstdonoharm.dev)";
     case "The Hippocratic License 3.0":
-      return "https://firstdonoharm.dev";
+      return "[https://firstdonoharm.dev](https://firstdonoharm.dev)";
     case "IBM Public License Version 1.0":
-      return "https://opensource.org/licenses/IPL-1.0";
+      return "[https://opensource.org/licenses/IPL-1.0](https://opensource.org/licenses/IPL-1.0)";
     case "ISC License (ISC)":
-      return "https://opensource.org/licenses/ISC";
+      return "[https://opensource.org/licenses/ISC](https://opensource.org/licenses/ISC)";
     case "The MIT License":
-      return "https://opensource.org/licenses/MIT";
+      return "[https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)";
     case "Mozilla Public License 2.0":
-      return "https://opensource.org/licenses/MPL-2.0";
+      return "[https://opensource.org/licenses/MPL-2.0](https://opensource.org/licenses/MPL-2.0)";
     case "Attribution License (BY)":
-      return "https://opendatacommons.org/licenses/by/";
+      return "[https://opendatacommons.org/licenses/by/](https://opendatacommons.org/licenses/by/)";
     case "Open Database License (ODbL)":
-      return "https://opendatacommons.org/licenses/odbl/";
+      return "[https://opendatacommons.org/licenses/odbl/](https://opendatacommons.org/licenses/odbl/)";
     case "Public Domain Dedication and License (PDDL)":
-      return "https://opendatacommons.org/licenses/pddl/";
+      return "[https://opendatacommons.org/licenses/pddl/](https://opendatacommons.org/licenses/pddl/)";
     case "The Perl License":
-      return "https://opensource.org/licenses/Artistic-2.0";
+      return "[https://opensource.org/licenses/Artistic-2.0](https://opensource.org/licenses/Artistic-2.0)";
     case "The Artistic License 2.0":
-      return "https://opensource.org/licenses/Artistic-2.0";
+      return "[https://opensource.org/licenses/Artistic-2.0](https://opensource.org/licenses/Artistic-2.0)";
     case "SIL Open Font License 1.1":
-      return "https://opensource.org/licenses/OFL-1.1";
+      return "[https://opensource.org/licenses/OFL-1.1](https://opensource.org/licenses/OFL-1.1)";
     case "The Unlicense":
-      return "http://unlicense.org/";
+      return "[http://unlicense.org/](http://unlicense.org/)";
     case "The Do What the Fuck You Want to Public License":
-      return "http://www.wtfpl.net/about/";
+      return "[http://www.wtfpl.net/about/](http://www.wtfpl.net/about/)";
     case "The zlib/libpng License":
-      return "https://opensource.org/licenses/Zlib";
+      return "[https://opensource.org/licenses/Zlib](https://opensource.org/licenses/Zlib)";
     default:
       return "";
   }
@@ -147,17 +147,38 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license) return "## License";
+  if (license)
+    return `## License
+  
+    ${renderLicenseLink(data.license)}
+  
+    ## Badges
+  
+    ${renderLicenseBadge(data.license)}`;
   else return "";
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseSection(data.title)}
-  ${renderLicenseLink(data.title)}
-  ${renderLicenseBadge(data.title)}
-`;
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
